64c979a41da9921fd0726b1c44efca690b4b7a09
Pirlo972 <53042@supinfo.com>
2020-12-11 12:13:28 +0100

    End of section 9

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     children: [
       {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]}, 
-      {path: 'memebrs/:id', component: MemberDetailComponent},
+      {path: 'members/:username', component: MemberDetailComponent},
       {path: 'lists', component: ListsComponent},
       {path: 'messages', component: MessagesComponent}
     ]
